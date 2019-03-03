da54177894769464646fba4a1db08f852b448992
Dalsen Chung <Codedalsen@163.com>
2019-03-03 23:55:42 +0800

    commit .sql files

--- stuTeaApp.sql
+++ stuTeaApp.sql
@@ -25,12 +25,13 @@ CREATE TABLE IF NOT EXISTS `admin_user_tb` (
   `admin_account` varchar(50) NOT NULL DEFAULT '0' COMMENT '管理员账号',
   `admin_password` varchar(50) NOT NULL DEFAULT '0' COMMENT '管理员密码',
   PRIMARY KEY (`admin_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='管理员基础信息表';
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COMMENT='管理员基础信息表';
 
 -- 正在导出表  teastucnct.admin_user_tb 的数据：~0 rows (大约)
 /*!40000 ALTER TABLE `admin_user_tb` DISABLE KEYS */;
 INSERT INTO `admin_user_tb` (`admin_id`, `admin_name`, `college_id`, `major_id`, `admin_account`, `admin_password`) VALUES
-	(1, '温雪莹', 1, 1, '152014060', 'e10adc3949ba59abbe56e057f20f883e');
+	(1, '温雪莹', 1, 1, '152014060', 'e10adc3949ba59abbe56e057f20f883e'),
+	(2, 'Dalsen', 1, 3, '142014119', '123');
 /*!40000 ALTER TABLE `admin_user_tb` ENABLE KEYS */;
 
 -- 导出  表 teastucnct.apply_record_tb 结构
@@ -53,29 +54,53 @@ CREATE TABLE IF NOT EXISTS `apply_record_tb` (
 
 -- 导出  表 teastucnct.college_info_tb 结构
 CREATE TABLE IF NOT EXISTS `college_info_tb` (
-  `college_id` int(11) NOT NULL COMMENT '学院id',
+  `college_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '学院id',
   `college_name` varchar(200) NOT NULL COMMENT '学院名称',
   `college_icon` varchar(100) NOT NULL COMMENT '学院图标',
   `college_address` varchar(300) NOT NULL COMMENT '学院地址',
-  `college_introduction` varchar(300) NOT NULL COMMENT '学院简介',
+  `college_introduction` varchar(500) NOT NULL COMMENT '学院简介',
   PRIMARY KEY (`college_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='学院基础信息表';
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COMMENT='学院基础信息表';
 
 -- 正在导出表  teastucnct.college_info_tb 的数据：~0 rows (大约)
 /*!40000 ALTER TABLE `college_info_tb` DISABLE KEYS */;
+INSERT INTO `college_info_tb` (`college_id`, `college_name`, `college_icon`, `college_address`, `college_introduction`) VALUES
+	(1, '电子学院', '6553b73c621dc7cce07191df6c38fdb6.png', '二教', '123'),
+	(2, '管理学院', '6553b73c621dc7cce07191df6c38fdb6.png', '四教', '123');
 /*!40000 ALTER TABLE `college_info_tb` ENABLE KEYS */;
+
+-- 导出  表 teastucnct.edu_branch_info_tb 结构
+CREATE TABLE IF NOT EXISTS `edu_branch_info_tb` (
+  `edu_branch_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '部门ID',
+  `edu_branch_name` varchar(50) NOT NULL DEFAULT '0' COMMENT '部门名称',
+  `edu_branch_address` varchar(200) NOT NULL DEFAULT '0' COMMENT '部门地址',
+  `edu_branch_introduction` varchar(500) NOT NULL DEFAULT '0' COMMENT '部门简介',
+  PRIMARY KEY (`edu_branch_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COMMENT='教学及科研部门信息表';
+
+-- 正在导出表  teastucnct.edu_branch_info_tb 的数据：~0 rows (大约)
+/*!40000 ALTER TABLE `edu_branch_info_tb` DISABLE KEYS */;
+INSERT INTO `edu_branch_info_tb` (`edu_branch_id`, `edu_branch_name`, `edu_branch_address`, `edu_branch_introduction`) VALUES
+	(2, '1', '2', '3');
+/*!40000 ALTER TABLE `edu_branch_info_tb` ENABLE KEYS */;
 
 -- 导出  表 teastucnct.major_info_tb 结构
 CREATE TABLE IF NOT EXISTS `major_info_tb` (
-  `major_id` int(11) NOT NULL COMMENT '专业id',
+  `major_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '专业id',
   `college_id` int(11) NOT NULL COMMENT '所属学院id',
   `major_name` varchar(50) NOT NULL COMMENT '专业名称',
   `major_introduction` varchar(300) NOT NULL COMMENT '专业简介',
   PRIMARY KEY (`major_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='专业信息表';
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COMMENT='专业信息表';
 
--- 正在导出表  teastucnct.major_info_tb 的数据：~0 rows (大约)
+-- 正在导出表  teastucnct.major_info_tb 的数据：~5 rows (大约)
 /*!40000 ALTER TABLE `major_info_tb` DISABLE KEYS */;
+INSERT INTO `major_info_tb` (`major_id`, `college_id`, `major_name`, `major_introduction`) VALUES
+	(1, 1, '通信工程', '1'),
+	(3, 1, '计算机软件与技术', '1'),
+	(4, 2, '人力资源', '2'),
+	(5, 2, '金融管理', '3'),
+	(6, 2, '行政管理', '4');
 /*!40000 ALTER TABLE `major_info_tb` ENABLE KEYS */;
 
 -- 导出  表 teastucnct.notice_tb 结构
@@ -88,6 +113,8 @@ CREATE TABLE IF NOT EXISTS `notice_tb` (
   `notice_read_times` int(11) NOT NULL DEFAULT '0' COMMENT '公告被浏览次数',
   `college_id` int(11) NOT NULL COMMENT '可查看的学院ID',
   `major_id` int(11) NOT NULL COMMENT '可查看的专业ID',
+  `notice_pass_review` int(11) NOT NULL COMMENT '是否通过审核',
+  `notice_is_all` int(11) DEFAULT NULL COMMENT '是否发送至全校',
   PRIMARY KEY (`notice_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='公告表';
 
@@ -139,14 +166,17 @@ CREATE TABLE IF NOT EXISTS `stu_user_tb` (
   `stu_grade` varchar(50) NOT NULL DEFAULT '0' COMMENT '年级',
   `college_id` int(11) NOT NULL DEFAULT '0' COMMENT '学院Id',
   `major_id` int(11) NOT NULL DEFAULT '0' COMMENT '专业Id',
-  `class_id` varchar(50) NOT NULL DEFAULT '0' COMMENT '班级',
+  `stu_class` varchar(50) NOT NULL DEFAULT '0' COMMENT '班级',
   `stu_phone` varchar(50) NOT NULL DEFAULT '0' COMMENT '电话',
   `stu_address` varchar(200) NOT NULL DEFAULT '0' COMMENT '家庭住址',
   PRIMARY KEY (`stu_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='学生用户基本信息表';
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COMMENT='学生用户基本信息表';
 
--- 正在导出表  teastucnct.stu_user_tb 的数据：~0 rows (大约)
+-- 正在导出表  teastucnct.stu_user_tb 的数据：~2 rows (大约)
 /*!40000 ALTER TABLE `stu_user_tb` DISABLE KEYS */;
+INSERT INTO `stu_user_tb` (`stu_id`, `role_id`, `stu_account`, `stu_password`, `stu_name`, `stu_sex`, `stu_grade`, `college_id`, `major_id`, `stu_class`, `stu_phone`, `stu_address`) VALUES
+	(1, 1, '142014119', 'e10adc3949ba59abbe56e057f20f883e', 'dalsen', '男', '2014级', 1, 4, '二班', '15622281569', '123'),
+	(3, 1, '151', '2', '2', '男', '2', 1, 3, '23', '321', '321');
 /*!40000 ALTER TABLE `stu_user_tb` ENABLE KEYS */;
 
 -- 导出  表 teastucnct.tea_user_tb 结构
@@ -163,10 +193,12 @@ CREATE TABLE IF NOT EXISTS `tea_user_tb` (
   `tea_course` varchar(200) DEFAULT NULL COMMENT '所有教授课程，以逗号分隔',
   `tea_isInstructor` int(2) DEFAULT NULL COMMENT '是否为辅导员',
   PRIMARY KEY (`tea_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='教师用户基本信息表';
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COMMENT='教师用户基本信息表';
 
 -- 正在导出表  teastucnct.tea_user_tb 的数据：~0 rows (大约)
 /*!40000 ALTER TABLE `tea_user_tb` DISABLE KEYS */;
+INSERT INTO `tea_user_tb` (`tea_id`, `role_id`, `tea_account`, `tea_password`, `tea_name`, `tea_sex`, `college_id`, `major_id`, `tea_phone`, `tea_course`, `tea_isInstructor`) VALUES
+	(2, 1, '152014060', 'e10adc3949ba59abbe56e057f20f883e', '1', '男', 1, 3, '22', '22', 1);
 /*!40000 ALTER TABLE `tea_user_tb` ENABLE KEYS */;
 
 -- 导出  表 teastucnct.user_role_tb 结构
